4c5b080827a56f6a7787d387a8bc73ffe5337b1a
jonasmartin82 <166035538+jonasmartin82@users.noreply.github.com>
2024-07-14 13:33:41 +0200

    Update 5_Versuch_5.py

--- pages/5_Versuch_5.py
+++ pages/5_Versuch_5.py
@@ -69,11 +69,11 @@ with col4:
     
 # xxx #
 
-if "Zeichnen" not in st.session_state:
-    st.session_state["Zeichnen"] = False
+if "Zeichnen (Versuch 5)" not in st.session_state:
+    st.session_state["Zeichnen (Versuch 5)"] = False
 
-if "Erklärung" not in st.session_state:
-    st.session_state["Erklärung"] = False
+if "Erklärung (Versuch 5)" not in st.session_state:
+    st.session_state["Erklärung (Versuch 5)"] = False
  
 # xxx # 
 
@@ -83,8 +83,8 @@ with colA:
     st.write('')
     
 with colB:
-    if st.button(label='Zeichnen', type='primary'):
-        st.session_state['Zeichnen'] = not st.session_state['Zeichnen']
+    if st.button(label='Zeichnen (Versuch 5)', type='primary'):
+        st.session_state['Zeichnen (Versuch 5)'] = not st.session_state['Zeichnen (Versuch 5)']
     
 with colC:
     st.write('')
@@ -105,7 +105,7 @@ plt.grid()
 
 col1, col2, col3 = st.columns([0.2, 0.4, 0.2])
 
-if st.session_state['Zeichnen']:
+if st.session_state['Zeichnen (Versuch 5)']:
     with col1:
         st.write('')
     with col2:
@@ -123,13 +123,13 @@ if st.session_state['Zeichnen']:
 
 col1, col2, col3 = st.columns([0.45, 0.1, 0.45])
 
-if st.session_state['Zeichnen']:
+if st.session_state['Zeichnen (Versuch 5)']:
     with col1:
         st.write('')
     with col2:
         st.write('')
-        if st.button(label='Erklärung'):
-            st.session_state['Erklärung'] = not st.session_state['Erklärung']
+        if st.button(label='Erklärung (Versuch 5)'):
+            st.session_state['Erklärung (Versuch 5)'] = not st.session_state['Erklärung (Versuch 5)']
     with col3:
         st.write('')
     
@@ -137,7 +137,7 @@ if st.session_state['Zeichnen']:
 
 # ------------------------------------------------------------------------------------------------------------------------ #
 
-if st.session_state['Erklärung']:
+if st.session_state['Erklärung (Versuch 5)']:
     
     st.subheader('Erklärung:')
     
